5f31aaa8c4b5ce7cdf961bc1483f69aca778794d
RobnettSean22 <robnettsean22@gmail.com>
2021-11-24 00:28:52 +0900

    functions for initalizing and launching windows when apps on and close

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 const { app, BrowserWindow } = require("electron");
+
+const creatWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  creatWindow();
+  //macOS apps generally continue running even without any windows open, and activating the app when no windows are available should open a new one.
+  app.whenReady().then(() => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
